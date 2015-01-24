e4263873e7f15f3485e14bea35d5f8cd6e25dfff
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-01-24 20:58:56 +0100

    fix URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ronda/routing-clout "0.1.0-SNAPSHOT"
   :description "ronda RouteDescriptor for clout."
-  :url "https://github.com/xsc/ronda/tree/master/ronda-routing-bidi"
+  :url "https://github.com/xsc/ronda-routing-bidi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
