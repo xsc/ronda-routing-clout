dd9090d5c07496eac9d40e1899b39e2d9c29c6e5
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-02-27 09:45:02 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ronda/routing-clout "0.1.0"
+(defproject ronda/routing-clout "0.1.1-SNAPSHOT"
   :description "ronda RouteDescriptor for clout."
   :url "https://github.com/xsc/ronda-routing-clout"
   :license {:name "Eclipse Public License"
